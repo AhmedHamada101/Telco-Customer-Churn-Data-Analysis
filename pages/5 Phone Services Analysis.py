d2337818f7f62c94d3e6abab2f79e8a4cd83e716
Ahmed Hamada <120568782+AhmedHamada101@users.noreply.github.com>
2023-02-16 15:07:51 +0200

    Update 5 Phone Services Analysis.py

--- pages/5 Phone Services Analysis.py
+++ pages/5 Phone Services Analysis.py
@@ -11,10 +11,7 @@ def load_data():
 
 data = load_data()
 
-# add title
-st.markdown('<h1 style = "text-align:center; font-size:250%;">Telco Customare Churn Data Analysis</h1>', unsafe_allow_html = True)
-st.title(' ')
-st.title(' ')
+
 
 # function for exploring categorical feature
 def exploring_categorical_features_by_churn(feature): 
