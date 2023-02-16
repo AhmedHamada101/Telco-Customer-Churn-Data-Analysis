0926d125f72e7b22d9eef6d41f0789de68201a46
Ahmed Hamada <120568782+AhmedHamada101@users.noreply.github.com>
2023-02-16 14:32:11 +0200

    Update 4 Customer Status Analysis.py

--- pages/4 Customer Status Analysis.py
+++ pages/4 Customer Status Analysis.py
@@ -21,7 +21,7 @@ def exploring_categorical_features_by_churn(feature):
     return fig
 
 # add header
-st.markdown('### The effect of Gender, SeniorCitizen, Partner and Dependentson on customer churn:')
+st.markdown('### Impact of Gender, SeniorCitizen, Partner and Dependentson on customer churn:')
 st.header(' ')
 st.header(' ')
 
